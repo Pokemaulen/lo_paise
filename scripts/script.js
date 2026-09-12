f797d80986a8e46db302002967735a90322e6911
Martin1095 <mart.maulen@duocuc.cl>
2026-09-12 16:44:20 -0300

    nuevaCosa

--- scripts/script.js
+++ scripts/script.js
@@ -35,9 +35,32 @@
         salida.textContent = "Tú no puedes pasar a la frontera";
           imagen.src = "imagenes/trump.jpg"; // Ruta de la imagen
           imagen.style.display = "block"; // Mostrar la imagen
-        } else {
+        } else if (nacionalidad === "japonesa") {
         salida.textContent = "Tú eres bienvenido";
           imagen.src = "imagenes/Cafe.jpg"; // Imagen por defecto
           imagen.style.display = "block"; // Mostrar la imagen
-        }
+        } else if (nacionalidad === "cachorro") {
+        salida.textContent = "Tú eres bienvenido";
+          imagen.src = "imagenes/cachorro.jpg"; // Imagen por defecto
+          imagen.style.display = "block"; // Mostrar la imagen
+        }else {
+        salida.textContent = "Tu no puede entrar";
+          imagen.src = "imagenes/rudol.jpeg"; // Imagen por defecto
+          imagen.style.display = "block"; // Mostrar la imagen
     }
+  }
+
+
+
+  function header() {
+    document.getElementById("header").innerHTML = `
+
+        <section>
+          <img src='imagenes/novias.gif' alt='Novias' width="200" height="200">
+        </section>
+        `;
+
+  }
+
+  header();
+
